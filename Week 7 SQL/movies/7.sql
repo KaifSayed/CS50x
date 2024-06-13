422ab77122f3c7bb54ccfe6e9338ba7807047e24
Kaif <146644970+KaifSayed@users.noreply.github.com>
2024-06-13 19:52:17 +0530

    Add files via upload

---
+++ Week 7 SQL/movies/7.sql
@@ -0,0 +1,5 @@
+SELECT movies.title, ratings.rating FROM movies
+JOIN ratings ON ratings.movie_id = movies.id
+WHERE ratings.rating IS NOT NULL
+AND year = 2010
+ORDER BY ratings.rating DESC, movies.title ASC;
